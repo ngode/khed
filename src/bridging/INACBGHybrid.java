25116e4cf19b457b7bb72030d0d51b3abf16a046
aandialfian <saiful8180@students.amikom.ac.id>
2017-10-05 23:44:42 +0700

    Malam jum'at, bukan waktunya garap sebenarnya

--- src/bridging/INACBGHybrid.java
+++ src/bridging/INACBGHybrid.java
@@ -17,7 +17,10 @@ import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
@@ -45,6 +48,7 @@ import javax.swing.JPanel;
 import javax.swing.JProgressBar;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
+import simrskhanza.DlgHybrid;
 
 /**
  *
@@ -167,6 +171,28 @@ public class INACBGHybrid extends javax.swing.JDialog {
     }
  
     public void loadURL(String url) {  
+        
+        Properties prop = new Properties();
+        try
+        {
+            prop.loadFromXML(new FileInputStream("setting/database.xml"));
+        }
+        catch (IOException ex)
+        {
+            Logger.getLogger(DlgHybrid.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        String portApache = prop.getProperty("PORTAPACHE");
+        if (portApache == null || portApache.isEmpty())
+            portApache = "";
+        else
+            portApache = ":" + portApache;
+        
+        String host = "http://"+prop.getProperty("HOST") + "/";
+        String hostEd = "http://"+prop.getProperty("HOST") + portApache + "/";
+        
+        final String urlEd = url.replace(host, hostEd);
+        
         try {
             createScene();
         } catch (Exception e) {
@@ -174,9 +200,9 @@ public class INACBGHybrid extends javax.swing.JDialog {
         
         Platform.runLater(() -> {
             try {
-                engine.load(url);
+                engine.load(urlEd);
             }catch (Exception exception) {
-                engine.load(url);
+                engine.load(urlEd);
             }
         });        
     }    
